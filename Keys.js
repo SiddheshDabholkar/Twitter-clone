5952f68ed04f00fae0d76c5ca403fc5cd55fa7c1
siddhesh <iamsiddhesh22@gmail.com>
2021-06-04 23:42:57 +0530

    register user backend

--- Keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGODB:
-    "mongodb://user:user@cluster0-shard-00-00.pyrgw.mongodb.net:27017,cluster0-shard-00-01.pyrgw.mongodb.net:27017,cluster0-shard-00-02.pyrgw.mongodb.net:27017/merng?ssl=true&replicaSet=atlas-4qt0hu-shard-0&authSource=admin&retryWrites=true&w=majority",
-  SECRET_KEY: "iamsiddhesh",
-};
