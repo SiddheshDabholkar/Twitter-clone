91f58917f64faa22ed32cc01fa424aa25e182329
siddhesh <iamsiddhesh22@gmail.com>
2021-07-24 17:41:16 +0530

    added z-index

--- client/src/components/Modals/ModalUtils.jsx
+++ client/src/components/Modals/ModalUtils.jsx
@@ -13,6 +13,7 @@ export const Bg = styled.div`
   top: 0;
   bottom: 0;
   right: 0;
+  z-index: 1000001;
   @media (max-width: 450px) {
     margin: 0px;
     width: 100%;
@@ -26,6 +27,7 @@ export const ModalContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  z-index: 1000000;
   position: fixed;
   width: ${({ width }) => width};
   left: 0;
