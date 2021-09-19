62bafb4249faba292e771760f537037614483078
siddhesh <iamsiddhesh22@gmail.com>
2021-09-19 19:53:08 +0530

    added api key n secret key

--- client/src/hooks/useUploadImage.jsx
+++ client/src/hooks/useUploadImage.jsx
@@ -4,6 +4,8 @@ export default function useUploadImage(selectedPhoto) {
   const [assetUrl, setAssetUrl] = useState("");
   const UPLOAD_PRESET = process.env.REACT_APP_UPLOAD_PRESET;
   const CLOUDINARY_NAME = process.env.REACT_APP_CLOUDINARY_NAME;
+  const CLOUDINARY_API = process.env.REACT_APP_API_KEY;
+  const CLOUDINARY_API_SECRET = process.env.REACT_APP_API_SECRET;
 
   useEffect(() => {
     if (selectedPhoto !== "") {
@@ -11,6 +13,8 @@ export default function useUploadImage(selectedPhoto) {
       data.append("file", selectedPhoto);
       data.append("upload_preset", UPLOAD_PRESET);
       data.append("cloud_name", CLOUDINARY_NAME);
+      data.append("api_key", CLOUDINARY_API);
+      data.append("api_secret", CLOUDINARY_API_SECRET);
       fetch(`https://api.cloudinary.com/v1_1/${CLOUDINARY_NAME}/image/upload`, {
         method: "post",
         body: data,
