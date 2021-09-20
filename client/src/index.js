3deb8351db8a4a82801bafe8a5ce53097d623d4c
siddhesh <iamsiddhesh22@gmail.com>
2021-09-20 18:33:27 +0530

    changed httplink

--- client/src/index.js
+++ client/src/index.js
@@ -10,15 +10,9 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
-const url =
-  process.env.NODE_ENV === "production"
-    ? "https://tweeetr.herokuapp.com/"
-    : "http://localhost:5000";
-
 const httpLink = new HttpLink({
   // uri: "http://localhost:5000",
-  // uri: "https://tweeetr.herokuapp.com/",
-  uri: url,
+  uri: "https://tweeetr.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
