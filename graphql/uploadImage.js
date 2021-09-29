3a1578575c7c5dda63fcfcf66f4114b39e1282b4
siddhesh <iamsiddhesh22@gmail.com>
2021-09-29 11:53:30 +0530

    resolved upload image error

--- graphql/uploadImage.js
+++ graphql/uploadImage.js
@@ -1,10 +1,9 @@
 const cloudinary = require("cloudinary").v2;
-require("dotenv").config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "drntday51",
+  api_key: "119814624233554",
+  api_secret: "kjkSq6TF7JsYkaPVzrrpVJfvYEs",
   secure: true,
 });
 
