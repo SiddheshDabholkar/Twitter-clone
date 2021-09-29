f9c73e45ec8a930811ff535889eaee18e1b46e84
siddhesh <iamsiddhesh22@gmail.com>
2021-09-29 11:45:05 +0530

    added variables directly

--- graphql/uploadImage.js
+++ graphql/uploadImage.js
@@ -1,10 +1,16 @@
 const cloudinary = require("cloudinary").v2;
-require("dotenv").config();
+// require("dotenv").config();
 
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+//   secure: true,
+// });
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "drntday51",
+  api_key: "119814624233554",
+  api_secret: "kjkSq6TF7JsYkaPVzrrpVJfvYEs",
   secure: true,
 });
 
